f7ab158675e083f743193b6b58239b07ce5cd070
JayeshLad <jrebh.9@gmail.com>
2024-03-06 20:27:12 -0500

    Changed api endpoint to /item

--- src/main/java/com/example/BHListing/controller/ItemController.java
+++ src/main/java/com/example/BHListing/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.example.BHListing.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,8 @@ import com.example.BHListing.model.Item;
 import com.example.BHListing.service.ItemService;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/item")
+@CrossOrigin
 public class ItemController {
 	
 	@Autowired
